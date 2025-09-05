8a19918e95fa9580a5dbcb033dcb171026b9bffd
ILLYA-code <illya.official.20@outlook.com>
2025-09-05 18:41:13 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './gw_test/templates/**/*.html',
+    './mainapp/templates/**/*.html',
+    './templates/**/*.html',
+    './static/src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

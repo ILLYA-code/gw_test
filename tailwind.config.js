4c40fd0ae647911b38c31df12abee4a3426a0eb0
ILLYA-code <illya.official.20@outlook.com>
2025-09-08 10:52:27 +0300

    first working result

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,32 @@ module.exports = {
     './templates/**/*.html',
     './static/src/**/*.js',
   ],
+  safelist: [
+    'py-[8px]',
+    'px-[12px]',
+    'font-bold',
+    'hover:bg-gray-200',
+    'cursor-pointer',
+    'text-[14px]',
+    'gap-[10px]',
+    'hover:bg-[#F4F4F4]',
+    'underline',
+    'w-[60px]',
+    'h-[70px]',
+    'object-cover',
+    'text-[#747474]',
+    'h-10',
+    'bg-[#d9d9d9]',
+    'p-2.5',
+    'duration-2000',
+    'font-bold'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mulish: ['Mulish', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
